13a10526246f355764f85fda346d78078a1419b0
humade <hayfordkojo18@gmail.com>
2023-10-19 05:31:43 +0000

    Add main function

---
+++ 0-main.c
@@ -0,0 +1,16 @@
+#include "stdio.h"
+#include "stdlib.h"
+#include "unistd.h"
+#include "main.h"
+/**
+ * main - runs it all
+ * @argc: number of arguments
+ * @argv: an array of pointers to strings
+ *
+ * Description: this is where the program starts and ends
+ * Return: always 0.
+ */
+int main(int argc, char *argv[])
+{
+	return (0);
+}
